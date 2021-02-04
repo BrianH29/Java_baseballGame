540ab1fe019b83ff7966d2a104eff50e7d6bf58e
brian <brianhwang29@gmail.com>
2021-02-04 14:11:17 +0900

    run game

--- src/com/bh/Run.java
+++ src/com/bh/Run.java
@@ -1,11 +1,27 @@
 package com.bh;
 
+import com.bh.baseballGame.Result;
 import com.bh.baseballGame.Setup;
 
 public class Run {
 
     public static void main(String[] args) {
         Setup set = new Setup();
-        set.randomNumber();
+        int cnt = 0;
+
+        int[] randomNo = set.randomNumber();
+
+        while (true) {
+            cnt++;
+            int[] inputNo = set.inputNumber();
+            Result result = set.compareNumber(randomNo, inputNo);
+
+            set.printResult(result);
+
+            if (set.endGame(result)) {
+                System.out.printf("it took %d turns to finish", cnt);
+                break;
+            }
+        }
     }
 }
